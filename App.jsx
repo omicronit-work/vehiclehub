15240e0b20276f53d3dda94baa039a88054b2670
armanbijoy <armanbijoy4@gmail.com>
2026-02-05 12:59:39 +0600

    SplashScreen with timer Implemented

--- App.jsx
+++ App.jsx
@@ -1,11 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import SplashScreen from './src/components/SplashScreen'
 
 const App = () => {
+  // State to control splash screen visibility
+  const [isSplash, setIsSplash] = useState(true)
+
+  // useEffect to handle splash screen timer
+  useEffect(() => {
+   
+    const timer = setTimeout(() => {
+      setIsSplash(false);
+    }, 3000);
+
+    // Cleanup function to clear timer on component unmount
+    return () => clearTimeout(timer);
+  }, []); 
+
+  // Render splash screen if isSplash is true
+  if (isSplash) return <SplashScreen />;
+
+  // Main app content after splash screen
   return (
-  <>
-       <SplashScreen/>
+       <>
+       <Text>Hello</Text>
        </>
   )
 }
